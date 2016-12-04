fb5d113fdc96b82b57413c7e78b0fca3725f280d
Jaden Wang <jadenyjw@gmail.com>
2016-12-04 19:14:48 +0000

    Delete unnecessary writes.

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -9,6 +9,7 @@ int rightB = 13;
 
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(9600);
     pinMode(enableLeft, OUTPUT);
     pinMode(enableRight, OUTPUT);
  
@@ -16,44 +17,43 @@ void setup() {
     pinMode(leftB, OUTPUT);
     pinMode(rightA, OUTPUT);
     pinMode(rightB, OUTPUT);
+    
+    stop();
+    
+    digitalWrite(leftA, HIGH);
+    digitalWrite(leftB, LOW);
+    digitalWrite(rightA, HIGH);
+    digitalWrite(rightB, LOW);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
 
+  
 }
 
 void forward(){
-  analogWrite(enableLeft, 255)
-  analogWrite(enableRight, 255)
+  analogWrite(enableLeft, 255);
+  analogWrite(enableRight, 255);
   
-  digitalWrite(leftA, HIGH);
-  digitalWrite(leftB, LOW);
-  digitalWrite(rightA, HIGH);
-  digitalWrite(rightB, LOW);
+
 }
 void left(){
   
-  analogWrite(enableLeft, 153)
-  analogWrite(enableRight, 255)
+  analogWrite(enableLeft, 153);
+  analogWrite(enableRight, 255);
   
-  digitalWrite(leftA, HIGH);
-  digitalWrite(leftB, LOW);
-  digitalWrite(rightA, HIGH);
-  digitalWrite(rightB, LOW);
 }
 void right(){
-  analogWrite(enableLeft, 255)
-  analogWrite(enableRight, 153)
   
-  digitalWrite(leftA, HIGH);
-  digitalWrite(leftB, LOW);
-  digitalWrite(rightA, HIGH);
-  digitalWrite(rightB, LOW);
+  analogWrite(enableLeft, 255);
+  analogWrite(enableRight, 153);
+
 }
 void stop(){
-  analogWrite(enableLeft, 0)
-  analogWrite(enableRight, 0)
+  analogWrite(enableLeft, 0);
+  analogWrite(enableRight, 0);
 }
 
